098aaa665cc4cfde469f438d79eefb491c3300e6
rodrigoemcode <rodrigosilva.emcode@gmail.com>
2025-04-24 11:19:28 -0300

    url.py and views.py update

--- projeto/urls.py
+++ projeto/urls.py
@@ -9,14 +9,20 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path°('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+from recipes.views import home, contato, sobre
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), # home
+    path('sobre/', sobre), # /sobre/
+    path('contato/', contato) # /contato/
 ]
---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME')
+
+def contato(request):
+    return HttpResponse('Contato')
+
+def sobre(request):
+    return HttpResponse('Sobre')
