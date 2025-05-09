d91b6e3d32402f292d2aab434634314ac8ff03c5
rodrigoemcode <rodrigosilva.emcode@gmail.com>
2025-05-09 19:15:09 -0300

    style(recipe-cards): update layout, typography and hover states

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from recipes.models import Recipe  # Ajuste o import conforme seus models
+
+class RecipeModelTest(TestCase):
+    def setUp(self):
+        self.recipe = Recipe.objects.create(name="Bolo de Chocolate")
+
+    def test_recipe_creation(self):
+        self.assertEqual(self.recipe.name, "Bolo de Chocolate")
+
+class HomeViewTest(TestCase):
+    def test_home_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
